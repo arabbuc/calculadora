201f243890ad310cad97c1e46badf19a90c4ea80
Armando <you@example.com>
2020-02-11 09:19:15 +0100

    ejercicio calculadora springboot

---
+++ src/main/java/com/abella/armando/holaSpring/HolaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.abella.armando.holaSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolaSpringApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/abella/armando/holaSpring/controller/MainController.java
@@ -0,0 +1,64 @@
+package com.abella.armando.holaSpring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import ch.qos.logback.core.pattern.util.RestrictedEscapeUtil;
+
+/**
+ * Hola
+ */
+@Controller
+public class MainController {
+
+    @GetMapping ("/")
+    @ResponseBody
+    public String HolaMundo(@RequestParam("Nombre") String name,@RequestParam("edad") String edad  ){
+
+        return "Hola: "+ name + "Tienes:"+Integer.parseInt(edad)+" años";
+
+    }
+    @GetMapping("/nuevo")
+    public ModelAndView helloHtml(){
+        ModelAndView respuesta= new ModelAndView("hello");
+        String msg="Hola Mundo html";
+        respuesta.addObject("mensaje", msg);
+        return respuesta;
+    }
+//**************************************************************** */
+    @GetMapping("/calculator")
+    @ResponseBody
+    public String  calculator(@RequestParam("op1") String op1,
+    @RequestParam("op2") String op2, @RequestParam("operador") String operador)
+        {
+        String result;
+        Double cosa;
+        String salida="";
+        switch (operador) {
+            case "s":
+                cosa=Double.parseDouble(op1)+Double.parseDouble(op2);
+            break;
+            case "-":
+                cosa=Double.parseDouble(op1)-Double.parseDouble(op2);
+            break;
+            case "*":
+                cosa=Double.parseDouble(op1)*Double.parseDouble(op2);
+            break;
+            case "/":
+                cosa=Double.parseDouble(op1)/Double.parseDouble(op2);
+            break;
+        
+            default:
+                cosa=0.0;
+                break;
+        }
+        result=cosa.toString();
+        salida="El resultado de la operación es: "+ result;
+        return salida;
+
+    }
+/*******************************************************************/
+}
